eeeddbf9447e10a4c717e6e9056554b0e8818e77
mahong125 <m.hong125@gmail.com>
2015-11-24 22:36:12 +0800

    add at 20151124

---
+++ Test_20151124/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_20151124
+//
+//  Created by mahong on 15/11/24.
+//  Copyright © 2015年 mahong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test_20151124/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test_20151124
+//
+//  Created by mahong on 15/11/24.
+//  Copyright © 2015年 mahong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
